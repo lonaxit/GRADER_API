be584b49251b48c8929b4eb48ad9f81eefb099e8
lonaxit <lonaxit@outlook.com>
2023-07-20 17:49:01 +0100

    Update tasks.py
    Added task to upload class

--- core/tasks.py
+++ core/tasks.py
@@ -48,15 +48,29 @@ def migrate_academic_session(data):
         except TypeError as e:
             raise TypeError(f"Type error: {e}")
 
-    
 @shared_task
-def upload_loan_deduction(data):
-    # data = json.loads(data)
-    # convert the JSON data to a DataFrame
+def migrate_school_class(data):
+  
     data_frame = pd.read_json(data)
-    print(data_frame)
-    print(3 * 7)
-    
+ 
+    with transaction.atomic():
+        try:
+            for row in data_frame.itertuples():
+
+                # _date_stamp=row.entry_date
+                # _date_timestamp_ms = int(_date_stamp) / 1000
+                # _date = datetime.datetime.utcfromtimestamp(_date_timestamp_ms)
+                
+                SchoolClass.objects.create(
+                    class_name = row.session,
+                    code = row.code
+                )
+                    
+        except ValueError as e:
+            raise ValueError(f"Invalid value: {e}")
+        except TypeError as e:
+            raise TypeError(f"Type error: {e}")  
+
     
 # @shared_task
 # def createLoanDeductions(userid):
