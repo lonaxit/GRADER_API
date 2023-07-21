8c65d0de3cf3401bc31ec8b0e2a0666c6d5d7f2e
lonaxit <lonaxit@outlook.com>
2023-07-21 17:00:57 +0100

    Update tasks.py
    update date format again

--- core/tasks.py
+++ core/tasks.py
@@ -124,9 +124,9 @@ def migrate_users_task(data):
     with transaction.atomic():
         try:
             for row in data_frame.itertuples():
-                # _date_stamp=row.dob
-                # _date_timestamp_ms = int(_date_stamp) / 1000
-                # _date = datetime.datetime.utcfromtimestamp(_date_timestamp_ms)
+                _date_stamp=row.dob
+                _date_timestamp_ms = int(_date_stamp) / 1000
+                _date = datetime.datetime.utcfromtimestamp(_date_timestamp_ms)
                 
                 userObj = User.objects.create(
                         username = data_frame.student_username,
@@ -135,7 +135,7 @@ def migrate_users_task(data):
                         other_name =  data_frame.other_name,
                         gender = data_frame.sex,
                         phone = data_frame.user_id,
-                        dob = row.dob,
+                        dob = _date,
                         is_staff = False,
                         is_student = True,
                                     )
